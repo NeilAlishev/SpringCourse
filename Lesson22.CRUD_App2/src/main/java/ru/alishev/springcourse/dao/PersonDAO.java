8baaa4dc032fc38d6d09020b193542da6cfcfd59
Neil Alishev <alishev.neil@gmail.com>
2020-09-16 15:59:13 +0300

    Add Lesson 22 code

---
+++ Lesson22.CRUD_App2/src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,38 @@
+package ru.alishev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.alishev.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Neil Alishev
+ */
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+}
---
+++ Lesson22_Starter.CRUD_App2/src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -0,0 +1,36 @@
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.alishev.springcourse.dao.PersonDAO;
+
+/**
+ * @author Neil Alishev
+ */
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
