3877ac5a1ee06de7139a7b74b761a26916924e38
Neil Alishev <alishev.neil@gmail.com>
2021-02-08 18:42:15 +0300

    Add Lesson 40 code

---
+++ Lesson40.PatternValidation/src/main/java/ru/alishev/springcourse/controllers/BatchController.java
@@ -0,0 +1,38 @@
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.alishev.springcourse.dao.PersonDAO;
+
+/**
+ * @author Neil Alishev
+ */
+@Controller
+@RequestMapping("/test-batch-update")
+public class BatchController {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public BatchController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index() {
+        return "batch/index";
+    }
+
+    @GetMapping("/without")
+    public String withoutBatch() {
+        personDAO.testMultipleUpdate();
+        return "redirect:/people";
+    }
+
+    @GetMapping("/with")
+    public String withBatch() {
+        personDAO.testBatchUpdate();
+        return "redirect:/people";
+    }
+}
---
+++ Lesson40.PatternValidation/src/main/java/ru/alishev/springcourse/util/PersonValidator.java
@@ -0,0 +1,42 @@
+package ru.alishev.springcourse.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.alishev.springcourse.dao.PersonDAO;
+import ru.alishev.springcourse.models.Person;
+
+/**
+ * @author Neil Alishev
+ */
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+        if (personDAO.show(person.getEmail()).isPresent()) {
+            // поле, код ошибки, сообщение ошибки
+            errors.rejectValue("email", "", "This email is already in use");
+        }
+
+        // Проверяем, что у человека имя начинается с заглавной буквы
+        // Если имя не начинается с заглавной буквы - выдаем ошибку
+        if (!Character.isUpperCase(person.getName().codePointAt(0)))
+            errors.rejectValue("name", "", "Name should start with a capital letter");
+    }
+}
