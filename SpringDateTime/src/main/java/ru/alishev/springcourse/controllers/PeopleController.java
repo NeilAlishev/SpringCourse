1e77946d82c11175d07c58bad1fae9de5ced41d9
Neil Alishev <alishev.neil@gmail.com>
2021-05-05 21:42:45 +0300

    Add date and time app

---
+++ SpringDateTime/src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -0,0 +1,75 @@
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.alishev.springcourse.models.Person;
+import ru.alishev.springcourse.services.PeopleService;
+
+import javax.validation.Valid;
+
+/**
+ * @author Neil Alishev
+ */
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PeopleService peopleService;
+
+    @Autowired
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", peopleService.findAll());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", peopleService.findOne(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+
+        peopleService.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", peopleService.findOne(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
+        peopleService.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        peopleService.delete(id);
+        return "redirect:/people";
+    }
+}
