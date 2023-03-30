762a336860d1ceb1f63400ffccf481c93f431d3f
SGoriainov <wishmast777@gmail.com>
2023-03-30 21:27:10 +0400

    lesson 26,27,29 update

--- Lesson29.JdbcTemplateBatchUpdate/src/main/java/ru/alishev/springcourse/config/SpringConfig.java
+++ Lesson29.JdbcTemplateBatchUpdate/src/main/java/ru/alishev/springcourse/config/SpringConfig.java
@@ -62,7 +62,7 @@ public class SpringConfig implements WebMvcConfigurer {
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
         dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setPassword("postgre");
 
         return dataSource;
     }
